05120e3962b871f8284d5813ed513adf04f5ac7b
Zbigniew Klasik <zklasik@future-processing.com>
2016-10-27 09:09:30 +0200

    add Bitretsmah.UI.ConsoleApp

---
+++ src/Bitretsmah.UI.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Bitretsmah.UI.ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Bitretsmah!");
+            Console.ReadKey();
+        }
+    }
+}
