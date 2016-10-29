13112f6b3523e12bb33470875f67bc3de1058f47
Zbigniew Klasik <zbigniew.klasik@gmail.com>
2016-10-29 15:36:53 +0200

    sample settings

--- src/Bitretsmah.UI.ConsoleApp/Program.cs
+++ src/Bitretsmah.UI.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fclp;
+using System;
 
 namespace Bitretsmah.UI.ConsoleApp
 {
@@ -6,8 +7,59 @@ namespace Bitretsmah.UI.ConsoleApp
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello Bitretsmah!");
-            Console.ReadKey();
+            var parser = new FluentCommandLineParser<ApplicationArguments>();
+
+            parser.SetupHelp("h", "help").Callback(text => Console.WriteLine(text)).UseForEmptyArgs();
+            parser.Setup(x => x.ShowVersion).As('v', "version").WithDescription("Shows the version of that application.");
+            parser.Setup(x => x.AddAccount).As("add-account").WithDescription("Adds a storage account.");
+            parser.Setup(x => x.AddUpdate).As("update-account").WithDescription("Updates a storrage account.");
+            parser.Setup(x => x.Silent).As('s', "silent").WithDescription("Runs with the silent mode.");
+            parser.Setup(x => x.Force).As('F', "froced").WithDescription("Runs with the forced mode."); ;
+
+            var result = parser.Parse(args);
+
+            if (result.HasErrors == false)
+            {
+                if (parser.Object.ShowVersion) ShowVersion();
+                if (parser.Object.AddAccount) AddAccount();
+                if (parser.Object.AddUpdate) UpdateAccount();
+            }
+            else
+            {
+                Console.WriteLine(result.ErrorText);
+            }
+
+            foreach (var keyValuePair in result.AdditionalOptionsFound)
+            {
+                Console.WriteLine($"AdditionalOptionsFound: key: {keyValuePair.Key} value: {keyValuePair.Value}");
+            }
+
+            Console.WriteLine("Bitretsmah finished!");
         }
+
+        private static void ShowVersion()
+        {
+            Console.WriteLine("ShowVersion");
+        }
+
+        private static void AddAccount()
+        {
+            Console.WriteLine("AddAccount");
+        }
+
+        private static void UpdateAccount()
+        {
+            Console.WriteLine("UpdateAccount");
+        }
+    }
+
+    public class ApplicationArguments
+    {
+        public bool ShowVersion { get; set; }
+        public bool AddAccount { get; set; }
+        public bool AddUpdate { get; set; }
+
+        public bool Silent { get; set; }
+        public bool Force { get; set; }
     }
 }
