7d9b32650e56e22395c5674c51bf9e3b867c854c
Zbigniew Klasik <zbigniew.klasik@gmail.com>
2016-11-07 22:12:48 +0100

    first changes in MegaStore

--- src/Bitretsmah.Data.Files/MegaStore.cs
+++ src/Bitretsmah.Data.Files/MegaStore.cs
@@ -1,5 +1,6 @@
 ﻿using Bitretsmah.Core.Interfaces;
 using Bitretsmah.Core.Models;
+using CG.Web.MegaApiClient;
 using System;
 using System.Net;
 using System.Threading.Tasks;
@@ -8,24 +9,34 @@ namespace Bitretsmah.Data.Mega
 {
     public class MegaStore : IRemoteFileStore
     {
-        private NetworkCredential _credentia;
+        private readonly NetworkCredential _credentia;
+        private readonly IMegaApiClient _megaApiClient;
 
         public MegaStore(NetworkCredential credential)
         {
             _credentia = credential;
+            _megaApiClient = new MegaApiClient();
         }
 
         public string StoreId => _credentia.UserName;
 
         public Quota Quota { get; private set; }
 
-        public Task<RemoteId> UploadFile(string localFilePath, IProgress<double> progress)
+        public async Task<RemoteId> UploadFile(string localFilePath, IProgress<double> progress)
         {
+            INode parentNode = null;
+            INode node = await _megaApiClient.UploadFileAsync(localFilePath, parentNode, progress);
+
+            // todo update quota
             throw new NotImplementedException();
         }
 
-        public Task DownloadFile(RemoteId remoteId, string localFilePath, IProgress<double> progress)
+        public async Task DownloadFile(RemoteId remoteId, string localFilePath, IProgress<double> progress)
         {
+            INode node = null;
+
+            await _megaApiClient.DownloadFileAsync(node, localFilePath, progress);
+
             throw new NotImplementedException();
         }
     }
