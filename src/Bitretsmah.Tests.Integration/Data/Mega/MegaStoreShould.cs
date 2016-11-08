5ef1acf9d2ba03d9a93a233242648a5226a1bfcb
Zbigniew Klasik <zbigniew.klasik@gmail.com>
2016-11-08 21:55:44 +0100

    set up MegaStore tests

---
+++ src/Bitretsmah.Tests.Integration/Data/Mega/MegaStoreShould.cs
@@ -0,0 +1,40 @@
+﻿using CG.Web.MegaApiClient;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Net;
+
+namespace Bitretsmah.Tests.Integration.Data.Mega
+{
+    [TestFixture]
+    public class MegaStoreShould
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            CleanUpMegaAccount(AppConfigHelper.GetTestMegaCredential());
+        }
+
+        [Test]
+        public void Test()
+        {
+            Console.WriteLine("test");
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            CleanUpMegaAccount(AppConfigHelper.GetTestMegaCredential());
+        }
+
+        public void CleanUpMegaAccount(NetworkCredential credential)
+        {
+            var megaApiClient = new MegaApiClient();
+            megaApiClient.Login(credential.UserName, credential.Password);
+            var nodes = megaApiClient.GetNodes().ToList();
+            nodes.Where(n => n.Type == NodeType.File).ToList().ForEach(n => megaApiClient.Delete(n, false));
+            nodes.Where(n => n.Type == NodeType.Directory).ToList().ForEach(n => megaApiClient.Delete(n, false));
+            megaApiClient.Logout();
+        }
+    }
+}
