f912c73a14f47f0c823e5fd9b4a3e4be5317421f
Zbigniew Klasik <zbigniew.klasik@gmail.com>
2016-11-15 20:03:39 +0100

    test helpers refactor

--- src/Bitretsmah.Tests.Unit/Core/NodeChangesFinderShould.cs
+++ src/Bitretsmah.Tests.Unit/Core/NodeChangesFinderShould.cs
@@ -3,7 +3,7 @@ using Bitretsmah.Core.Models;
 using FluentAssertions;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
+using static Bitretsmah.Tests.Unit.Core.NodesTestHelper;
 
 namespace Bitretsmah.Tests.Unit.Core
 {
@@ -17,8 +17,8 @@ namespace Bitretsmah.Tests.Unit.Core
         [Test]
         public void FindNoChangeForSameFiles()
         {
-            var initialFile = CreateF("foo.txt");
-            var finalFile = CreateF("foo.txt");
+            var initialFile = CreateFile("foo.txt");
+            var finalFile = CreateFile("foo.txt");
             var result = _finder.Find(initialFile, finalFile);
             result.State.Should().Be(NodeState.None);
         }
@@ -26,8 +26,8 @@ namespace Bitretsmah.Tests.Unit.Core
         [Test]
         public void FindModifedForDifferentFileSize()
         {
-            var initialFile = CreateF("foo.txt");
-            var finalFile = CreateF("foo.txt");
+            var initialFile = CreateFile("foo.txt");
+            var finalFile = CreateFile("foo.txt");
             initialFile.Size = 5555;
             var result = _finder.Find(initialFile, finalFile);
             result.State.Should().Be(NodeState.Modified);
@@ -36,8 +36,8 @@ namespace Bitretsmah.Tests.Unit.Core
         [Test]
         public void FindModifedForDifferentFileCreationTime()
         {
-            var initialFile = CreateF("foo.txt");
-            var finalFile = CreateF("foo.txt");
+            var initialFile = CreateFile("foo.txt");
+            var finalFile = CreateFile("foo.txt");
             initialFile.CreationTime = new DateTimeOffset(2016, 12, 12, 1, 1, 1, new TimeSpan(0));
             var result = _finder.Find(initialFile, finalFile);
             result.State.Should().Be(NodeState.Modified);
@@ -46,8 +46,8 @@ namespace Bitretsmah.Tests.Unit.Core
         [Test]
         public void FindModifedForDifferentFileModificationTime()
         {
-            var initialFile = CreateF("foo.txt");
-            var finalFile = CreateF("foo.txt");
+            var initialFile = CreateFile("foo.txt");
+            var finalFile = CreateFile("foo.txt");
             initialFile.ModificationTime = new DateTimeOffset(2016, 12, 12, 1, 1, 1, new TimeSpan(0));
             var result = _finder.Find(initialFile, finalFile);
             result.State.Should().Be(NodeState.Modified);
@@ -56,8 +56,8 @@ namespace Bitretsmah.Tests.Unit.Core
         [Test]
         public void FindNoChangeForNullFileHashes()
         {
-            var initialFile = CreateF("foo.txt");
-            var finalFile = CreateF("foo.txt");
+            var initialFile = CreateFile("foo.txt");
+            var finalFile = CreateFile("foo.txt");
             initialFile.Hash = null;
             finalFile.Hash = null;
             var result = _finder.Find(initialFile, finalFile);
@@ -67,8 +67,8 @@ namespace Bitretsmah.Tests.Unit.Core
         [Test]
         public void FindNoChangeForFirstNullFileHash()
         {
-            var initialFile = CreateF("foo.txt");
-            var finalFile = CreateF("foo.txt");
+            var initialFile = CreateFile("foo.txt");
+            var finalFile = CreateFile("foo.txt");
             initialFile.Hash = null;
             var result = _finder.Find(initialFile, finalFile);
             result.State.Should().Be(NodeState.None);
@@ -77,8 +77,8 @@ namespace Bitretsmah.Tests.Unit.Core
         [Test]
         public void FindNoChangeForSecondNullFileHash()
         {
-            var initialFile = CreateF("foo.txt");
-            var finalFile = CreateF("foo.txt");
+            var initialFile = CreateFile("foo.txt");
+            var finalFile = CreateFile("foo.txt");
             finalFile.Hash = null;
             var result = _finder.Find(initialFile, finalFile);
             result.State.Should().Be(NodeState.None);
@@ -87,8 +87,8 @@ namespace Bitretsmah.Tests.Unit.Core
         [Test]
         public void FindModifedForDifferentFileHashes()
         {
-            var initialFile = CreateF("foo.txt");
-            var finalFile = CreateF("foo.txt");
+            var initialFile = CreateFile("foo.txt");
+            var finalFile = CreateFile("foo.txt");
             finalFile.Hash = "different";
             var result = _finder.Find(initialFile, finalFile);
             result.State.Should().Be(NodeState.Modified);
@@ -101,8 +101,8 @@ namespace Bitretsmah.Tests.Unit.Core
         [Test]
         public void FindNoChangesInDirectory()
         {
-            var initialDirectory = CreateD("D", CreateF("F"));
-            var finalDirectory = CreateD("D", CreateF("F"));
+            var initialDirectory = CreateDirectory("D", CreateFile("F"));
+            var finalDirectory = CreateDirectory("D", CreateFile("F"));
             var result = (Directory)_finder.Find(initialDirectory, finalDirectory);
             result.State.Should().Be(NodeState.None);
             result.InnerNodes.Should().BeEmpty();
@@ -111,8 +111,8 @@ namespace Bitretsmah.Tests.Unit.Core
         [Test]
         public void FindCreatedFileInDirectory()
         {
-            var initialDirectory = CreateD("D", CreateF("F1"));
-            var finalDirectory = CreateD("D", CreateF("F1"), CreateF("F2"));
+            var initialDirectory = CreateDirectory("D", CreateFile("F1"));
+            var finalDirectory = CreateDirectory("D", CreateFile("F1"), CreateFile("F2"));
             var result = (Directory)_finder.Find(initialDirectory, finalDirectory);
             result.State.Should().Be(NodeState.Modified);
             result.InnerNodes.Count.Should().Be(1);
@@ -123,8 +123,8 @@ namespace Bitretsmah.Tests.Unit.Core
         [Test]
         public void FindModifiedFileInDirectory()
         {
-            var initialDirectory = CreateD("D", CreateF("F1"), CreateF("F2", NodeState.None, "hash"));
-            var finalDirectory = CreateD("D", CreateF("F1"), CreateF("F2", NodeState.None, "different_hash"));
+            var initialDirectory = CreateDirectory("D", CreateFile("F1"), CreateFile("F2", NodeState.None, "hash"));
+            var finalDirectory = CreateDirectory("D", CreateFile("F1"), CreateFile("F2", NodeState.None, "different_hash"));
             var result = (Directory)_finder.Find(initialDirectory, finalDirectory);
             result.State.Should().Be(NodeState.Modified);
             result.InnerNodes.Count.Should().Be(1);
@@ -135,8 +135,8 @@ namespace Bitretsmah.Tests.Unit.Core
         [Test]
         public void FindDeletedFileInDirectory()
         {
-            var initialDirectory = CreateD("D", CreateF("F1"), CreateF("F2"));
-            var finalDirectory = CreateD("D", CreateF("F1"));
+            var initialDirectory = CreateDirectory("D", CreateFile("F1"), CreateFile("F2"));
+            var finalDirectory = CreateDirectory("D", CreateFile("F1"));
             var result = (Directory)_finder.Find(initialDirectory, finalDirectory);
             result.State.Should().Be(NodeState.Modified);
             result.InnerNodes.Count.Should().Be(1);
@@ -152,23 +152,23 @@ namespace Bitretsmah.Tests.Unit.Core
         public void FindCreatedDirectoryAndMarkInnerNodes()
         {
             var initialNode =
-                CreateD("D_0",
-                    CreateF("F_1"));
+                CreateDirectory("D_0",
+                    CreateFile("F_1"));
 
             var finalNode =
-                CreateD("D_0",
-                    CreateF("F_1"),
-                    CreateD("D_1", // has been created
-                        CreateF("F_2"),
-                        CreateD("D_2",
-                            CreateF("F_3"))));
+                CreateDirectory("D_0",
+                    CreateFile("F_1"),
+                    CreateDirectory("D_1", // has been created
+                        CreateFile("F_2"),
+                        CreateDirectory("D_2",
+                            CreateFile("F_3"))));
 
             var expectedResult =
-                CreateD("D_0", NodeState.Modified,
-                    CreateD("D_1", NodeState.Created,
-                        CreateF("F_2", NodeState.Created),
-                        CreateD("D_2", NodeState.Created,
-                            CreateF("F_3", NodeState.Created))));
+                CreateDirectory("D_0", NodeState.Modified,
+                    CreateDirectory("D_1", NodeState.Created,
+                        CreateFile("F_2", NodeState.Created),
+                        CreateDirectory("D_2", NodeState.Created,
+                            CreateFile("F_3", NodeState.Created))));
 
             var actualResult = (Directory)_finder.Find(initialNode, finalNode);
             actualResult.ShouldBeEquivalentTo(expectedResult);
@@ -179,23 +179,23 @@ namespace Bitretsmah.Tests.Unit.Core
         public void FindDeletedDirectoryAndMarkInnerNodes()
         {
             var initialNode =
-                CreateD("D_0",
-                    CreateF("F_1"),
-                    CreateD("D_1", // will be deleted
-                        CreateF("F_2"),
-                        CreateD("D_2",
-                            CreateF("F_3"))));
+                CreateDirectory("D_0",
+                    CreateFile("F_1"),
+                    CreateDirectory("D_1", // will be deleted
+                        CreateFile("F_2"),
+                        CreateDirectory("D_2",
+                            CreateFile("F_3"))));
 
             var finalNode =
-                CreateD("D_0",
-                    CreateF("F_1"));
+                CreateDirectory("D_0",
+                    CreateFile("F_1"));
 
             var expectedResult =
-                CreateD("D_0", NodeState.Modified,
-                    CreateD("D_1", NodeState.Deleted,
-                        CreateF("F_2", NodeState.Deleted),
-                        CreateD("D_2", NodeState.Deleted,
-                            CreateF("F_3", NodeState.Deleted))));
+                CreateDirectory("D_0", NodeState.Modified,
+                    CreateDirectory("D_1", NodeState.Deleted,
+                        CreateFile("F_2", NodeState.Deleted),
+                        CreateDirectory("D_2", NodeState.Deleted,
+                            CreateFile("F_3", NodeState.Deleted))));
 
             var actualResult = (Directory)_finder.Find(initialNode, finalNode);
             actualResult.ShouldBeEquivalentTo(expectedResult);
@@ -206,65 +206,65 @@ namespace Bitretsmah.Tests.Unit.Core
         public void FindAllModifiedNodes()
         {
             var initialNode =
-                CreateD("root",
-                    CreateF("F_0"),
-                    CreateF("F_1"),                 // will be deleted
-                    CreateD("D_2",
-                        CreateF("F_2_0"),
-                        CreateF("F_2_1"),
-                        CreateD("D_2_2",
-                            CreateF("F_2_2_0"),
-                            CreateF("F_2_2_1")),    // will be deleted
-                        CreateD("D_2_3",            // will be deleted
-                            CreateF("F_2_3_0"),
-                            CreateF("F_2_3_1"))),
-                    CreateD("D_3",                  // will be deleted
-                        CreateF("F_3_0"),
-                        CreateF("F_3_1"),
-                        CreateD("D_3_2",
-                            CreateF("F_3_2_0"),
-                            CreateF("F_3_2_1")),
-                        CreateD("D_3_3",
-                            CreateF("F_3_3_0"),
-                            CreateF("F_3_3_1"))));
+                CreateDirectory("root",
+                    CreateFile("F_0"),
+                    CreateFile("F_1"),                          // will be deleted
+                    CreateDirectory("D_2",
+                        CreateFile("F_2_0"),
+                        CreateFile("F_2_1"),
+                        CreateDirectory("D_2_2",
+                            CreateFile("F_2_2_0"),
+                            CreateFile("F_2_2_1")),             // will be deleted
+                        CreateDirectory("D_2_3",                // will be deleted
+                            CreateFile("F_2_3_0"),
+                            CreateFile("F_2_3_1"))),
+                    CreateDirectory("D_3",                      // will be deleted
+                        CreateFile("F_3_0"),
+                        CreateFile("F_3_1"),
+                        CreateDirectory("D_3_2",
+                            CreateFile("F_3_2_0"),
+                            CreateFile("F_3_2_1")),
+                        CreateDirectory("D_3_3",
+                            CreateFile("F_3_3_0"),
+                            CreateFile("F_3_3_1"))));
 
             var finalNode =
-                CreateD("root",
-                    CreateF("F_0"),
-                    CreateD("D_2",
-                        CreateF("F_2_0"),
-                        CreateF("F_2_1", NodeState.None, "different_hash_1"),
-                        CreateD("D_2_2",
-                            CreateF("F_2_2_0", NodeState.None, "different_hash_2")),
-                            CreateD("D_2_2_2",      // has been created
-                                CreateF("F_2_2_2_0"),
-                                CreateD("F_2_2_2_1",
-                                    CreateF("F_2_2_2_1_0")))));
+                CreateDirectory("root",
+                    CreateFile("F_0"),
+                    CreateDirectory("D_2",
+                        CreateFile("F_2_0"),
+                        CreateFile("F_2_1", NodeState.None, "different_hash_1"),
+                        CreateDirectory("D_2_2",
+                            CreateFile("F_2_2_0", NodeState.None, "different_hash_2")),
+                            CreateDirectory("D_2_2_2",          // has been created
+                                CreateFile("F_2_2_2_0"),
+                                CreateDirectory("F_2_2_2_1",
+                                    CreateFile("F_2_2_2_1_0")))));
 
             var expectedResult =
-                CreateD("root", NodeState.Modified,
-                    CreateF("F_1", NodeState.Deleted),
-                    CreateD("D_2", NodeState.Modified,
-                        CreateF("F_2_1", NodeState.Modified, "different_hash_1"),
-                        CreateD("D_2_2", NodeState.Modified,
-                            CreateF("F_2_2_0", NodeState.Modified, "different_hash_2"),
-                            CreateF("F_2_2_1", NodeState.Deleted)),
-                            CreateD("D_2_2_2", NodeState.Created,
-                                CreateF("F_2_2_2_0", NodeState.Created),
-                                CreateD("F_2_2_2_1", NodeState.Created,
-                                    CreateF("F_2_2_2_1_0", NodeState.Created))),
-                        CreateD("D_2_3", NodeState.Deleted,
-                            CreateF("F_2_3_0", NodeState.Deleted),
-                            CreateF("F_2_3_1", NodeState.Deleted))),
-                    CreateD("D_3", NodeState.Deleted,
-                        CreateF("F_3_0", NodeState.Deleted),
-                        CreateF("F_3_1", NodeState.Deleted),
-                        CreateD("D_3_2", NodeState.Deleted,
-                            CreateF("F_3_2_0", NodeState.Deleted),
-                            CreateF("F_3_2_1", NodeState.Deleted)),
-                        CreateD("D_3_3", NodeState.Deleted,
-                            CreateF("F_3_3_0", NodeState.Deleted),
-                            CreateF("F_3_3_1", NodeState.Deleted))));
+                CreateDirectory("root", NodeState.Modified,
+                    CreateFile("F_1", NodeState.Deleted),
+                    CreateDirectory("D_2", NodeState.Modified,
+                        CreateFile("F_2_1", NodeState.Modified, "different_hash_1"),
+                        CreateDirectory("D_2_2", NodeState.Modified,
+                            CreateFile("F_2_2_0", NodeState.Modified, "different_hash_2"),
+                            CreateFile("F_2_2_1", NodeState.Deleted)),
+                            CreateDirectory("D_2_2_2", NodeState.Created,
+                                CreateFile("F_2_2_2_0", NodeState.Created),
+                                CreateDirectory("F_2_2_2_1", NodeState.Created,
+                                    CreateFile("F_2_2_2_1_0", NodeState.Created))),
+                        CreateDirectory("D_2_3", NodeState.Deleted,
+                            CreateFile("F_2_3_0", NodeState.Deleted),
+                            CreateFile("F_2_3_1", NodeState.Deleted))),
+                    CreateDirectory("D_3", NodeState.Deleted,
+                        CreateFile("F_3_0", NodeState.Deleted),
+                        CreateFile("F_3_1", NodeState.Deleted),
+                        CreateDirectory("D_3_2", NodeState.Deleted,
+                            CreateFile("F_3_2_0", NodeState.Deleted),
+                            CreateFile("F_3_2_1", NodeState.Deleted)),
+                        CreateDirectory("D_3_3", NodeState.Deleted,
+                            CreateFile("F_3_3_0", NodeState.Deleted),
+                            CreateFile("F_3_3_1", NodeState.Deleted))));
 
             var actualResult = (Directory)_finder.Find(initialNode, finalNode);
             actualResult.ShouldBeEquivalentTo(expectedResult);
@@ -272,43 +272,5 @@ namespace Bitretsmah.Tests.Unit.Core
         }
 
         #endregion NESTED DIRECTORIES
-
-        #region HELP METHODS
-
-        private DateTimeOffset newDate(int seconds)
-        {
-            return new DateTimeOffset(2016, 11, 12, 19, 34, seconds, new TimeSpan(0));
-        }
-
-        private File CreateF(string name, NodeState state = NodeState.None, string hash = "hash")
-        {
-            return new File
-            {
-                Name = name,
-                State = state,
-                Hash = hash,
-                Size = 1234,
-                CreationTime = new DateTimeOffset(2016, 10, 11, 18, 33, 14, new TimeSpan(0)),
-                ModificationTime = new DateTimeOffset(2016, 11, 12, 19, 34, 15, new TimeSpan(0)),
-                AbsolutePath = @"C:\Temp\" + name + ".txt"
-            };
-        }
-
-        private Directory CreateD(string name, NodeState state, params Node[] nodes)
-        {
-            return new Directory
-            {
-                Name = name,
-                State = state,
-                InnerNodes = new List<Node>(nodes)
-            };
-        }
-
-        private Directory CreateD(string name, params Node[] nodes)
-        {
-            return CreateD(name, NodeState.None, nodes);
-        }
-
-        #endregion HELP METHODS
     }
 }
---
+++ src/Bitretsmah.Tests.Unit/Core/NodesTestHelper.cs
@@ -0,0 +1,38 @@
+﻿using Bitretsmah.Core.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Bitretsmah.Tests.Unit.Core
+{
+    public static class NodesTestHelper
+    {
+        public static File CreateFile(string name, NodeState state = NodeState.None, string hash = "hash")
+        {
+            return new File
+            {
+                Name = name,
+                State = state,
+                Hash = hash,
+                Size = 1234,
+                CreationTime = new DateTimeOffset(2016, 10, 11, 18, 33, 14, new TimeSpan(0)),
+                ModificationTime = new DateTimeOffset(2016, 11, 12, 19, 34, 15, new TimeSpan(0)),
+                AbsolutePath = @"C:\Temp\" + name + ".txt"
+            };
+        }
+
+        public static Directory CreateDirectory(string name, NodeState state, params Node[] nodes)
+        {
+            return new Directory
+            {
+                Name = name,
+                State = state,
+                InnerNodes = new List<Node>(nodes)
+            };
+        }
+
+        public static Directory CreateDirectory(string name, params Node[] nodes)
+        {
+            return CreateDirectory(name, NodeState.None, nodes);
+        }
+    }
+}
