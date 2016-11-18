3b824da1e0c52f2e55348ded23a24533bf3cb88a
Zbigniew Klasik <zbigniew.klasik@gmail.com>
2016-11-18 21:06:06 +0100

    starting with NodeChangesApplier

---
+++ src/Bitretsmah.Core/NodeChangesApplier.cs
@@ -0,0 +1,55 @@
+﻿using Bitretsmah.Core.Models;
+using EnsureThat;
+using System;
+using System.Collections.Generic;
+
+namespace Bitretsmah.Core
+{
+    public interface INodeChangesApplier
+    {
+        Node Apply(Node initialNode, Node change);
+
+        Node Apply(Node initialNode, IEnumerable<Node> changes);
+    }
+
+    public class NodeChangesApplier : INodeChangesApplier
+    {
+        public Node Apply(Node initialNode, IEnumerable<Node> changes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Node Apply(Node initialNode, Node change)
+        {
+            if (initialNode is File && change is File)
+                return ApplyFileChange((File)initialNode, (File)change);
+
+            if (initialNode is Directory && change is Directory)
+                return ApplyDirectoryChange((Directory)initialNode, (Directory)change);
+
+            throw new InvalidOperationException("Unknown node type.");
+        }
+
+        private Node ApplyFileChange(File initialFile, File change)
+        {
+            Ensure.That(initialFile.Name).IsEqualTo(change.Name);
+            Ensure.That(initialFile.AbsolutePath).IsEqualTo(change.AbsolutePath);
+            Ensure.That(change.State == NodeState.Modified).IsTrue();
+
+            var finalNode = initialFile.DeepCopy();
+
+            finalNode.Hash = change.Hash;
+            finalNode.Size = change.Size;
+            finalNode.CreationTime = change.CreationTime;
+            finalNode.ModificationTime = change.ModificationTime;
+            finalNode.State = NodeState.None;
+
+            return finalNode;
+        }
+
+        private Node ApplyDirectoryChange(Directory initialDirectory, Directory change)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Bitretsmah.Tests.Unit/Core/NodeChangesApplierShould.cs
@@ -0,0 +1,47 @@
+﻿using Bitretsmah.Core;
+using Bitretsmah.Core.Models;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+
+namespace Bitretsmah.Tests.Unit.Core
+{
+    [TestFixture]
+    public class NodeChangesApplierShould
+    {
+        [Test]
+        public void ApplyFileModification()
+        {
+            var initialFile = new File
+            {
+                Name = "foo.txt",
+                State = NodeState.None,
+                Hash = "hash_1",
+                Size = 1234,
+                CreationTime = new DateTimeOffset(2015, 9, 10, 17, 32, 13, new TimeSpan(0)),
+                ModificationTime = new DateTimeOffset(2015, 9, 11, 18, 33, 14, new TimeSpan(0)),
+                AbsolutePath = @"C:\Temp\foo.txt"
+            };
+
+            var change = new File
+            {
+                Name = "foo.txt",
+                State = NodeState.Modified,
+                Hash = "hash_2",
+                Size = 4321,
+                CreationTime = new DateTimeOffset(2016, 10, 11, 18, 33, 14, new TimeSpan(0)),
+                ModificationTime = new DateTimeOffset(2016, 11, 12, 19, 34, 15, new TimeSpan(0)),
+                AbsolutePath = @"C:\Temp\foo.txt"
+            };
+
+            var expectedFile = change.DeepCopy();
+            expectedFile.State = NodeState.None;
+
+            INodeChangesApplier applier = new NodeChangesApplier();
+            var actualFile = applier.Apply(initialFile, change);
+
+            actualFile.ShouldBeEquivalentTo(expectedFile);
+            actualFile.ShouldSerializeSameAs(expectedFile);
+        }
+    }
+}
